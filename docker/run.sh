38550be14ca1c686991f14ac34ae6869b3b4a503
pokealimit <pokealimit@gmail.com>
2023-03-14 16:14:56 +0800

    map uid gid to container

--- docker/run.sh
+++ docker/run.sh
@@ -2,5 +2,6 @@
 docker run -it --rm \
     --name fast_lio \
     --network=host \
+    -u $(id -u):$(id -g) \
     -v $HOME/repo/FAST_LIO:/home/user/catkin_ws/src/FAST_LIO \
     fast-lio:noetic /bin/zsh -c "roslaunch fast_lio mapping_corriere.launch"
