77161c6e2948a32e18ac562afa1c788c33b283a9
aulnzhengscut <aulnzheng@sina.com>
2024-04-01 13:27:54 +0700

    for new robot

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-docker run -it --rm \
+# docker run -it --rm \
+docker run -itd \
     --name fast_lio \
     --network=host \
     -u $(id -u):$(id -g) \
-    -v $HOME/repo/FAST_LIO:/home/user/catkin_ws/src/FAST_LIO \
-    fast-lio:noetic /bin/zsh -c "roslaunch fast_lio mapping_corriere.launch"
+    -e ROS_MASTER_URI=http://10.0.0.41:11311/\
+    -e ROS_MASTER_PORT=11311\
+    -e ROS_IP=10.0.0.41\
+    -v $HOME/bumblebee_ZLN/FAST_LIO:/home/user/catkin_ws/src/FAST_LIO \
+    fast-lio:noetic /bin/zsh
